b77c5546a23b8605198e1c7abb687bdc4498dd81
Andrew <andrewvo1024@gmail.com>
2022-09-02 14:57:45 -0500

    Comment form and replies form done

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,3 +1,5 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .login_form import PostForm
+from .comments_form import Comments
+from .replies_form import Replies
---
+++ app/forms/comments_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Email, ValidationError, Length
+
+
+class Comments(FlaskForm):
+    body = StringField('body', validators=[DataRequired(), Length(min=1, max=140)])
+    images = StringField('images')
